f16b7cf26610ee6eb27426bbf25dd1a45e41659a
Welpike <106405084+Welpike@users.noreply.github.com>
2022-08-13 20:09:58 +0200

    Update script.js

--- leo/script.js
+++ leo/script.js
@@ -1,7 +1,7 @@
 // code a inclure dans la balise <head> de chaque page html dans une balise <script>
 
 connected = localStorage.getItem("is_connected");
-let delay = 1, login_url = new URL("https://eliaaa-webdev.github.io/leo/login.html");
+let delay = 1, login_url = new URL("https://eliaaa.000webhostapp.com/login.html");
 if(!connected){
   setTimeout("document.location.replace(login_url)",delay + '000',);
 }
