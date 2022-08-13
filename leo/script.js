86c550543dfd4b1478f97cef5d4871fac97e008f
Welpike <106405084+Welpike@users.noreply.github.com>
2022-08-13 19:03:59 +0200

    Create script.js

---
+++ leo/script.js
@@ -0,0 +1,5 @@
+connected = localStorage.getItem("is_connected");
+let delay = 1, login_url = new URL("https://eliaaa-webdev.github.io/leo/login.html");
+if(!connected){
+  setTimeout("document.location.replace(login_url)",delay + '000',);
+}
