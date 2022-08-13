ce83bee4fe883a5f4aaac6f98174a0a5cb53be05
Welpike <106405084+Welpike@users.noreply.github.com>
2022-08-13 19:08:38 +0200

    Update script.js

--- leo/script.js
+++ leo/script.js
@@ -1,3 +1,5 @@
+// code a inclure dans la balise <head> de chaque page html dans une balise <script>
+
 connected = localStorage.getItem("is_connected");
 let delay = 1, login_url = new URL("https://eliaaa-webdev.github.io/leo/login.html");
 if(!connected){
