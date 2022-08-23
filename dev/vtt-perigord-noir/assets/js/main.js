a15b49aedddffd728049360a70a10149d7ec6e8f
Welpike <106405084+Welpike@users.noreply.github.com>
2022-08-23 10:30:39 +0200

    Update main.js

--- dev/vtt-perigord-noir/assets/js/main.js
+++ dev/vtt-perigord-noir/assets/js/main.js
@@ -1,3 +1,5 @@
 // eliaaa-webdev
 
-let sta = localStorage.getItem('sta');if(sta==1){console.log(sta);localStorage.removeItem('sta');localStorage.setItem('sta', 2);}else if(sta==2){console.log(sta);localStorage.removeItem('sta');localStorage.setItem('sta', 3);}else if(sta==3){console.log(sta);localStorage.removeItem('sta');localStorage.setItem('sta', 4);}else if(sta==4){console.log(sta);localStorage.removeItem('sta');localStorage.setItem('sta', 5);}else if(sta==5){console.log(sta);localStorage.removeItem('sta');localStorage.setItem('sta', 6);}else{alert("Fermeture exceptionnelle le 23 août 2022 !");console.log(sta);localStorage.removeItem('sta');localStorage.setItem('sta', 1);}
+let sta = localStorage.getItem('sta');if(sta==1){localStorage.removeItem('sta');localStorage.setItem('sta', 2);}else if(sta==2){localStorage.removeItem('sta');localStorage.setItem('sta', 3);}else if(sta==3){localStorage.removeItem('sta');localStorage.setItem('sta', 4);}else if(sta==4){localStorage.removeItem('sta');localStorage.setItem('sta', 5);}else if(sta==5){localStorage.removeItem('sta');localStorage.setItem('sta', 6);}else{
+  // alert("Fermeture exceptionnelle le 23 août 2022 !");
+  localStorage.removeItem('sta');localStorage.setItem('sta', 1);}
